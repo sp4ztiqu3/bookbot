2df3b91a1cc5a88063b1c8b9b149416b656da913
Matt Hiddle <matt.hiddle@gmail.com>
2024-12-21 09:25:52 +0800

    implement nicer report format, and only count alpha characters

--- main.py
+++ main.py
@@ -7,16 +7,42 @@ def word_count(words):
     return count
 
 def char_count(words):
-    char_counts = {}
+    char_counts = []
     for char in words.lower():
-        if char not in char_counts:
-            char_counts[char] = 1
+        if not char.isalpha():
+            continue
+            
+        if len(char_counts) == 0:
+            char_counts.append({"character": char, "count": 1})
         else:
-            char_counts[char] += 1
+            found = False
+            for i in char_counts:
+                if i["character"] == char:
+                    i["count"] += 1
+                    found = True
+            
+            if not found:
+                char_counts.append({"character": char, "count": 1})
 
     return char_counts
 
-with open("books/frankenstein.txt") as f:
+def sort_dicts(item):
+    return item["count"]
+
+file_path = "books/frankenstein.txt"
+with open(file_path) as f:
     file_contents = f.read()
-    print(word_count(file_contents))
-    print(char_count(file_contents))
+    words = word_count(file_contents)
+    chars = char_count(file_contents)
+
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{words} words found in the document")
+    print("")
+
+    chars = sorted(chars, reverse=True, key=lambda a: a["count"])
+
+    for char in chars:
+        print(f"The '{char['character']}' character was found {char['count']} times")
+
+    print("--- End report ---")
+
