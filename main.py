8331ba28a9cf0f04a70fa949943213bf0ac103dc
Matt Hiddle <matt.hiddle@gmail.com>
2024-12-21 09:00:33 +0800

    add character counting function

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def word_count(words):
 
     return count
 
+def char_count(words):
+    char_counts = {}
+    for char in words.lower():
+        if char not in char_counts:
+            char_counts[char] = 1
+        else:
+            char_counts[char] += 1
+
+    return char_counts
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     print(word_count(file_contents))
+    print(char_count(file_contents))
